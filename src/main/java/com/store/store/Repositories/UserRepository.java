51fbeb44bc0ab1f525995e2524f281fd0ae4d771
Javier <javierondicol84@gmail.com>
2024-06-06 14:32:42 -0300

    fixed user table name

--- src/main/java/com/store/store/Repositories/UserRepository.java
+++ src/main/java/com/store/store/Repositories/UserRepository.java
@@ -2,8 +2,8 @@ package com.store.store.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.store.store.Entities.User;
+import com.store.store.Entities.ClientUser;
 
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface UserRepository extends JpaRepository<ClientUser, Long>{
     
 }

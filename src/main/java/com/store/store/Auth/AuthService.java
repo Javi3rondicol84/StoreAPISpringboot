3b0b66f52276074f0451bf082e15035c918f1920
Javier <javierondicol84@gmail.com>
2024-07-31 16:02:33 -0300

    login working, register it needs to be fixed

--- src/main/java/com/store/store/Auth/AuthService.java
+++ src/main/java/com/store/store/Auth/AuthService.java
@@ -1,8 +1,11 @@
 package com.store.store.Auth;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.store.store.Jwt.JwtService;
 import com.store.store.User.Role;
@@ -15,13 +18,27 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthService {
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+
+    private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
     public AuthResponse login(LoginRequest request) {
-        return null;
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+        UserDetails user = userRepository.findUserEntityByUserName(request.getUsername()).orElseThrow();
+        if(user != null) {
+            System.out.println("xd");
+        }
+        else {
+            System.out.println("sssssss");
+        }
+        String token = jwtService.getToken(user);
+        System.out.println("login"+request.getPassword());
+        System.out.println("login"+user.getPassword());
+        return AuthResponse.builder()
+              .token(token)
+              .build();
     }
 
     public AuthResponse register(RegisterRequest request) {
